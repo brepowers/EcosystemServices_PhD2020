f898967ba9d6d8f6edd75268f9dad9fdcb99fffa
brepowers <brepowers@users.noreply.github.com>
2024-01-24 14:58:56 -0700

    Update FacotrAnalysis_Rscrip.R

--- FacotrAnalysis_Rscrip.R
+++ FacotrAnalysis_Rscrip.R
@@ -1,8 +1,1 @@
-library(qrfactor)
-##Performing Fator Analysis with GIS data
-##https://rdrr.io/cran/qrfactor/man/qrfactor.html
 
-##Other Sources for factor Analysis
-##https://web.stanford.edu/class/psych253/tutorials/FactorAnalysis.html
-##http://rtutorialseries.blogspot.co.nz/2011/10/r-tutorial-series-exploratory-factor.html
-##https://www.r-bloggers.com/basic-factor-analysis-in-r/
